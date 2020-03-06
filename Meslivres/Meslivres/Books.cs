41fdbac690b8c861cde444a86729c770f2e41ca5
source*de*vie1 <gbamblekevin@gmail.com>
2020-03-06 15:58:50 +0000

    second commit

---
+++ Meslivres/Meslivres/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Meslivres
+{
+    public class Books
+    {
+        public string Name{ get; set;}
+        public int Price { get; set; }
+        public int Id { get; set; }
+
+        public Books()
+        {
+        }
+    }
+}
---
+++ Meslivres/Meslivres/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Meslivres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Books livreOne = new Books();
+            livreOne.Name = "Alladin";
+            livreOne.Price = 15000;
+            livreOne.Id = 1;
+
+            Books livreTwo = new Books();
+            livreTwo.Name = "petit prince";
+            livreTwo.Price = 23000;
+            livreTwo.Id = 2;
+
+            Books livreThree = new Books();
+            livreThree.Name = "TOm et jerry";
+            livreThree.Price = 3000;
+            livreThree.Id = 3;
+
+            Books livreFour = new Books();
+            livreFour.Name = "petit bodiel";
+            livreFour.Price = 2000;
+            livreFour.Id = 4;
+
+            List<Books> MaBiblio = new List<Books>() { livreOne, livreTwo, livreThree, livreFour };
+
+            var en = from l in MaBiblio where l.Id == 1 || l.Id == 2 || l.Id == 3 || l.Id == 4 select l;
+
+            foreach (var item in en)
+            {
+                Console.WriteLine("le roman "+" "+item.Name +" coute "+" "+ item.Price);
+            }
+
+        }
+    }
+}
